0e53c66b38c13aa9474e27a4c7ec1e67068ab56d
Kai Schell <kai.schell@signet.de>
2022-10-14 16:25:36 +0200

    refactoring: use functions

--- tictactoe.py
+++ tictactoe.py
@@ -4,45 +4,54 @@
 #  x | x | 0
 #  x | 0 | 0
 
-# print hello/intro statement
-print("Welcome to the awesome tictactoe game written in python!")
 
-# print board
-print("""
-- | - | -
-- | - | -
-- | - | -
-""")
-
-#  1 | 2 | 3
-#  4 | 5 | 6
-#  7 | 8 | 9
-
-# index   0 , 1, 2 ...
+## Variables that are used througout the code
 board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+position = 0
 
-# player 1 starts by placing his token on the board
-# player 1 places the 'X' symbol
-print("Player 1: Place your symbol on an empty space on the board.")
-position = input("Use a number between 1 and 9: ")
+## Function declaration/definitions (def)
+# print hello/intro statement
+def print_welcome():
+    print()
+    print("Welcome to the awesome tictactoe game written in python!")
+    print()
 
-# check user input
-if position.isdigit():
-    position = int(position)
-    # check if position is between 1 (inclusive) and 10 (exclusive)
-    if position in range(1, 10):
-        # translate user input to list index
-        # and update board / save state
-        board[position - 1] = 'X'
+def print_board():
+    print(board[0] + ' | ' + board[1] + ' | ' + board[2] + '\n'
+          + board[3] + ' | ' + board[4] + ' | ' + board[5] + '\n'
+          + board[6] + ' | ' + board[7] + ' | ' + board[8] + '\n')
+
+
+def check_user_input():
+    # use global variable postition so that we can
+    # change its value from inside the function
+    global position
+    if position.isdigit():
+        position = int(position)
+        # check if position is between 1 (inclusive) and 10 (exclusive)
+        if position in range(1, 10):
+            # translate user input to list index
+            # and update board / save state
+            board[position - 1] = 'X'
+        else:
+            print("Please enter a number between 1 and 9.")
     else:
         print("Please enter a number between 1 and 9.")
-else:
-   print("Please enter a number between 1 and 9.")
 
-# print board again
-print(board[0] + ' | ' + board[1] + ' | ' + board[2] + '\n'
-      + board[3] + ' | ' + board[4] + ' | ' + board[5] + '\n'
-      + board[6] + ' | ' + board[7] + ' | ' + board[8] + '\n')
+def get_user_input():
+    global position
+    print("Player 1: Place your symbol on an empty space on the board.")
+    position = input("Use a number between 1 and 9: ")
+
+
+## Function calls / actions
+print_welcome()
+print_board()
+# player 1 starts by placing his token on the board
+# player 1 places the 'X' symbol
+get_user_input()
+check_user_input()
+print_board()
 
 # now its player 2's turn:
 # place token
