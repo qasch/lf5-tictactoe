56faef88cfbd57769237d8f8208602a872b10d4b
Kai Schell <kai.schell@signet.de>
2022-10-14 14:32:34 +0200

    initial commit

---
+++ tictactoe.py
@@ -0,0 +1,31 @@
+# TicTacToe
+#
+#  x | 0 | x
+#  x | x | 0
+#  x | 0 | 0
+
+
+
+
+# print hello/intro statement
+print("Welcome to the awesome tictactoe game written in python!")
+print()
+
+# print board
+# player 1 starts by placing his token on the board
+# save state of board
+# print board again
+# now its player 2's turn:
+# place token
+# check if space is already taken
+# notify user if so
+# use loop to enable player to place token again if space is taken
+# save state
+# print board
+# NOTE: use some kind of loop!
+# check for win after every turn (starting with player 1)
+# check for tie after every turn (starting with player 1)
+# Loop until one player has won or there is a tie
+# print board one last time with apropriate message
+# ask user if it wants to play again
+
