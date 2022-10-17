f2b9894e78353699369e53b966a9d2a436bf0550
Kai Schell <kai.schell@signet.de>
2022-10-17 14:09:39 +0200

    basic working version without check for tie, no computer opponent

--- tictactoe.py
+++ tictactoe.py
@@ -16,6 +16,7 @@ def print_welcome():
     print("Welcome to the awesome tictactoe game written in python!")
     print()
 
+# TODO: maybe we can use a loop here as well?
 def print_board():
     print(board[0] + ' | ' + board[1] + ' | ' + board[2] + '\n'
           + board[3] + ' | ' + board[4] + ' | ' + board[5] + '\n'
@@ -69,6 +70,51 @@ def get_user_input():
     position = input("Use a number between 1 and 9: ")
 
 
+# TODO: Optimize code, maybe use a loop or something?
+def is_win_horizontal(current_player):
+   if board[0] == current_player and board[1] == current_player and board[2] == current_player:
+       return True
+   elif board[3] == current_player and board[4] == current_player and board[5] == current_player:
+       return True
+   elif board[6] == current_player and board[7] == current_player and board[8] == current_player:
+       return True
+   else:
+       return False
+
+
+def is_win_vertical(current_player):
+    if board[0] == current_player and board[3] == current_player and board[6] == current_player:
+        return True
+    elif board[1] == current_player and board[4] == current_player and board[7] == current_player:
+        return True
+    elif board[2] == current_player and board[5] == current_player and board[8] == current_player:
+        return True
+    else:
+        return False
+
+def is_win_diagonal(current_player):
+    if board[0] == current_player and board[4] == current_player and board[8] == current_player:
+        return True
+    elif board[6] == current_player and board[4] == current_player and board[2] == current_player:
+        return True
+    else:
+        return False
+
+
+def is_win(current_player):
+    if is_win_horizontal(current_player) or is_win_vertical(current_player) or is_win_diagonal(current_player):
+        # TODO: not the best place, better use dedicated function for it -> refactoring!
+        print("Player " + current_player + " has won!")
+        return True
+    else:
+        return False
+
+
+# TODO:
+def is_tie():
+    pass
+
+
 ## Function calls / actions
 
 current_player = 'X'
@@ -78,7 +124,9 @@ print_board()
 # player 1 places the 'X' symbol
 game_over = False
 # while game_over == False:
+# Loop until one player has won or there is a tie
 while not game_over:
+    # use loop to enable player to place token again if space is taken
     while True:
         get_user_input()
         if is_valid_input():   # returns True or False
@@ -87,21 +135,14 @@ while not game_over:
             print_board()
             break
 
+    # -> now its player 2's turn:
+    game_over = is_win(current_player)
     current_player = switch_player(current_player)
 
-        # checks for win or tie -> game_over = True
+    # checks for win or tie -> game_over = True
+    # check for win after every turn (starting with player 1)
+    # check for tie after every turn (starting with player 1)
 
-        # -> now its player 2's turn:
-        # place token
-        # check if space is already taken
-        # notify user if so
-        # use loop to enable player to place token again if space is taken
-        # save state
-        # print board
-        # NOTE: use some kind of loop!
-        # check for win after every turn (starting with player 1)
-        # check for tie after every turn (starting with player 1)
-        # Loop until one player has won or there is a tie
 
 # print board one last time with apropriate message
 # ask user if it wants to play again
